861126f1dadab96d4910f04987b4b96ec0d91952
frt043 <105953318+frt043@users.noreply.github.com>
2024-04-11 20:45:48 -0300

    vfv fdfvf

--- main.js
+++ main.js
@@ -15,4 +15,5 @@ for (let i = 0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date('2024-12-17');
+contadores[0].textContent = tempoObjetivo1;
 
