f4120d37bd03f4f2cd9006116b8782e2072372e8
frt043 <105953318+frt043@users.noreply.github.com>
2024-04-11 20:54:55 -0300

    grse

--- main.js
+++ main.js
@@ -15,5 +15,8 @@ for (let i = 0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date('2024-12-17T00:00:00');
+let tempoAtual = new Date();
+
 contadores[0].textContent = tempoObjetivo1;
+
 
