381e70c09a5239d9d0d010bcc26b17c6912a7d67
frt043 <105953318+frt043@users.noreply.github.com>
2024-04-11 20:55:27 -0300

    gndg

--- main.js
+++ main.js
@@ -17,6 +17,6 @@ const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date('2024-12-17T00:00:00');
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
 
 
