4c4d77b25bbd204e064b15024017d65711ddc757
frt043 <105953318+frt043@users.noreply.github.com>
2024-04-11 21:12:20 -0300

    gjuvh

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function calculaTempo(tempoObjetivo1) {
     let tempoFinal = tempoObjetivo1-tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(horas / minutos / 60);
+let horas = Math.floor(minutos / 60);
 let dias = Math.floor(dias / 24);
 return dias;
 }
