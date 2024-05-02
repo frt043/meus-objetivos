527f3a75f0daaf60d99b4ea9b82a6a99aa94b856
frt043 <105953318+frt043@users.noreply.github.com>
2024-05-02 19:46:49 -0300

    gbdg

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ let dias = Math.floor(horas / 24);
 segundos%= 60;
 minutos%= 60;
 horas%= 24;
-return dias; + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
+return dias + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
 }
 
 
