de63ae18b6f9752019b64134f4a5757616e57912
frt043 <105953318+frt043@users.noreply.github.com>
2024-05-02 20:29:26 -0300

    vdfbdfb

--- main.js
+++ main.js
@@ -23,12 +23,18 @@ const tempoObjetivo4 = new Date('2024-12-18T00:00:00');
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function atualizaCronometro(){
 for (let i = 0; i < contadores.length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i]);
 }
+}
+function comecaCronometro(){
 
 atualizaCronometro();
 setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();f
 
 
 function calculaTempo(tempoObjetivo1) {
