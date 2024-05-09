d36eb1085140a380f6fe1d756a67e53c2d1220bb
frt043 <105953318+frt043@users.noreply.github.com>
2024-05-09 19:52:38 -0300

    nfgjnz

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ minutos%= 60;
 horas%= 24;
 
 if (tempoFinal > 0) {
-return dias + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
+return dias + 'dias ' + horas + 'horas ' + minutos + 'minutos ' + segundos + 'segundos ';
 }
 
 else {
